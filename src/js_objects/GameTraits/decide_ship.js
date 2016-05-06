917bb1966f8f623fd33c6201a2e6bc6c4ab42993
Przemysław Drożdża <przemyslaw.drozdza@wiosna.org.pl>
2016-05-06 21:19:54 +0200

    changed player ship ammunition storage system

--- src/js_objects/GameTraits/decide_ship.js
+++ src/js_objects/GameTraits/decide_ship.js
@@ -3,7 +3,7 @@ GAMEobject.prototype.makeShipControlPanel = function(){
     var O = this.O[0];
     var S = this.SHIP;
     var Sx = this.SHIPold;
-    var html='';
+    var html='',modeName;
     S.speedM = (Math.sqrt(S.EnergyM))/(S.Weight/50);
 
     Sx.speed = -17;
@@ -25,7 +25,10 @@ GAMEobject.prototype.makeShipControlPanel = function(){
                 if(m == S.KeysModules[pimk][pomk])
                     letter='<div class="letter">'+String.fromCharCode(pimk)+'</div>';
 
-        modHtml+='<div class="cBox">'+letter+'<span id="moduleBox_'+m+'"><div class="energyBox min"><span class="smaller">Disabled</span><div class="infoBox">'+NAMES.ModName[S.Modules[m].T]+'</div></div></span></div>';
+        if(S.Modules[m].subT) modeName = NAMES.ModName[S.Modules[m].T+S.Modules[m].subT];
+                else          modeName = NAMES.ModName[S.Modules[m].T];
+
+        modHtml+='<div class="cBox">'+letter+'<span id="moduleBox_'+m+'"><div class="energyBox min"><span class="smaller">Disabled</span><div class="infoBox">'+modeName+'</div></div></span></div>';
 
     }
     html ='<div class="cBox"><div class="energyBox energyCap_00" id="modulesEnergyOut">'+S.Energy.toFixed(2)+'</div></div>'+modHtml+html;
@@ -37,12 +40,13 @@ GAMEobject.prototype.makeShipControlPanel = function(){
         html+='<div id="attackModule_'+fi+'" class="attackBox attack_'+S.FireTypes[fi].T+'"><span></span><span></span><span></span><span></span><span></span><div class="attackBoxNum">'+(fi- -1)+'</div></div>';
     }
 
-    if(S.MissleStorage > 0)
-        html+='<div class="ammoBox" id="missleStorage"></div>';
-    if(S.BombStorage > 0)
-        html+='<div class="ammoBox" id="bombStorage"></div>';
-    if(S.AmmoStorage > 0)
-        html+='<div class="ammoBox" id="ammoStorage"></div>';
+    Sx.Storage=[];
+    for(var Stype in S.Storage){
+        Sx.Storage[Stype]={R:0};
+        if(Stype=='Ammo' || Stype=='Bomb' || Stype=='Missile'){
+            html+='<div class="ammoBox" id="'+Stype+'Storage"></div>';
+        }
+    }
 
     html+='</div></div></div></div>';
 
@@ -64,7 +68,7 @@ GAMEobject.prototype.decide_ship = function(e){
     O.lastSpeedT = 0;
     this.mouseX = this.mouse_x- -(O.x -this.Dx/2);
     this.mouseY = this.mouse_y- -(O.y -this.Dy/2);
-    var html='';
+    var html='',modeName;
 
     if(this.specialMoveT==-1 && this.specialMove > 0){
         // Start special move
@@ -76,10 +80,10 @@ GAMEobject.prototype.decide_ship = function(e){
     }
 
     if(this.specialMoveT==-1){
-        if(this.keyLeftRight==1){    O.angle=(O.angle- -360 -O.speedT)%360;    O.lastSpeedT =-O.speedT; }
-        if(this.keyLeftRight==-1){    O.angle=(O.angle- -360- -O.speedT)%360;    O.lastSpeedT = O.speedT; }
-        if(this.keyUpDown==1)        O.speed-=-O.speedA/this.Frames;
-        if(this.keyUpDown==-1)        O.speed-=O.speedD/this.Frames;
+        if(this.keyLeftRight==1){  O.angle=(O.angle- -360 -O.speedT)%360;    O.lastSpeedT =-O.speedT; }
+        if(this.keyLeftRight==-1){ O.angle=(O.angle- -360- -O.speedT)%360;    O.lastSpeedT = O.speedT; }
+        if(this.keyUpDown==1)      O.speed-=-O.speedA/this.Frames;
+        if(this.keyUpDown==-1)     O.speed-=O.speedD/this.Frames;
     } else{
         // Do special move
 
@@ -98,7 +102,7 @@ GAMEobject.prototype.decide_ship = function(e){
         var M = S.Modules[m];
         modHtml='';
 
-        // Modu�y konfigurowane przy dezaktywacji:
+        // Moduły konfigurowane przy dezaktywacji:
         if(M.T=='laserProd' && M.Disabled == 1)
             M.LaserLoad=0;
 
@@ -106,9 +110,12 @@ GAMEobject.prototype.decide_ship = function(e){
             M.TeleLoad=0;
 
 
-        // Modu�y wy��czone:
+        // Moduły wyłączone:
         if(Sx.Mod[m] != 'disabled'){
-            $('#moduleBox_'+m).html('<div class="energyBox min"><span class="smaller">Disabled</span><div class="infoBox">'+NAMES.ModName[M.T]+'</div></div>');
+            if(M.subT) modeName = NAMES.ModName[M.T+M.subT];
+                else   modeName = NAMES.ModName[M.T];
+
+            $('#moduleBox_'+m).html('<div class="energyBox min"><span class="smaller">Disabled</span><div class="infoBox">'+modeName+'</div></div>');
             Sx.Mod[m]='disabled';
             if(M.T=='spotRegion') this.shipFunc_showSpotRegions(false);
         }
@@ -117,33 +124,35 @@ GAMEobject.prototype.decide_ship = function(e){
             continue;
 
 
-        // Modu�y niepotrzebne:
-        if(    (M.T=='bulletProd' && S.AmmoStorage > 0 && S.Ammo >=S.AmmoStorage )
-         ||    (M.T=='healerProd' && S.lifeM <= O.life)
-         || (M.T=='missleProd' && S.MissleStorage > 0 && S.Missles >=S.MissleStorage )
-         || (M.T=='bombProd' && S.BombStorage > 0 && S.Bombs >=S.BombStorage )
+        // Moduły niepotrzebne:
+        if( (M.T=='Prod' && S.Storage[ M.Storage ].R >= S.Storage[ M.Storage ].M)
+         || (M.T=='healerProd' && S.lifeM <= O.life)
          || (M.T=='esteemProd' && (F.T=='missle' || F.T=='missleR' || F.T=='laser'))
          || (M.T=='shieldProd' && S.EnergyFieldMax <= O.energyField) ){
             if(Sx.Mod[m] != 'done'){
-                $('#moduleBox_'+m).html('<div class="energyBox min">Done<div class="infoBox">'+NAMES.ModName[M.T]+'</div></div>');
+                if(M.subT) modeName = NAMES.ModName[M.T+M.subT];
+                    else   modeName = NAMES.ModName[M.T];
+                $('#moduleBox_'+m).html('<div class="energyBox min">Done<div class="infoBox">'+modeName+'</div></div>');
                 Sx.Mod[m]='done';
             }
             continue;
         }
 
-        // Modu�y bez energii:
+        // Moduły bez energii:
         if(M.Emin > S.Energy){
             M.E=0;
             if(M.T=='laserProd') M.LaserLoad = 0;
             if(M.T=='teleProd') M.TeleLoad = 0;
             if(Sx.Mod[m] != 0){
-                $('#moduleBox_'+m).html('<div class="energyBox min"><div class="min">min.</div>'+M.Emin.toFixed(2)+'<div class="infoBox">'+NAMES.ModName[M.T]+'</div></div>');
+                if(M.subT) modeName = NAMES.ModName[M.T+M.subT];
+                    else   modeName = NAMES.ModName[M.T];
+                $('#moduleBox_'+m).html('<div class="energyBox min"><div class="min">min.</div>'+M.Emin.toFixed(2)+'<div class="infoBox">'+modeName+'</div></div>');
                 Sx.Mod[m]=0;
                 if(M.T=='spotRegion') this.shipFunc_showSpotRegions(false);
             }
             continue;
         }
-        // Modu�y dzia�aj�ce:
+        // Moduły działające:
         if(M.T=='laserProd' && M.LaserLoad == M.LaserLoadM){
             if(Sx.Mod[m] != 'active'){
                 modHtml+='<div class="energyBox active"><div class="active">active</div>'+M.Emin.toFixed(2)+'<div class="infoBox">';
@@ -176,16 +185,18 @@ GAMEobject.prototype.decide_ship = function(e){
                 S.Energy-=M.Emax;
             }
         }
-        if(modHtml!='') modHtml+='<div class="titleBox">'+NAMES.ModName[M.T]+'</div>';
+        if(modHtml!=''){
+            if(M.subT) modeName = NAMES.ModName[M.T+M.subT];
+                else   modeName = NAMES.ModName[M.T];
+            modHtml+='<div class="titleBox">'+modeName+'</div>';
+        }
         var U = M.ProdX*(M.E/M.Emax);
 
         M.Prod-=-U;
         if(M.Prod > M.ifProd){
             if(M.T=='laserProd' && M.LaserLoad < M.LaserLoadM) ++M.LaserLoad;
             if(M.T=='teleProd' && M.TeleLoad < M.TeleLoadM) ++M.TeleLoad;
-            if(M.T=='bulletProd') ++S.Ammo;
-            if(M.T=='missleProd') ++S.Missles;
-            if(M.T=='bombProd')   ++S.Bombs;
+            if(M.T=='Prod') ++S.Storage [ M.Storage ].R;
             if(M.T=='healerProd'){
                 this.healObj(0,1);
             }
@@ -251,14 +262,17 @@ GAMEobject.prototype.decide_ship = function(e){
         this.shipFunc_glueFireToLaser();
 
     if(S.ShowAmmoIndicator){
-        if    (((F.T=='single' || F.T=='double' || F.T=='rose') && S.Ammo >= F.AmmoUse)
-                || ((F.T=='missle') && S.Missles >= F.MissleUse)
-                || ((F.T=='missleR') && S.Missles >= F.MissleUse)
-                || ((F.T=='bomb') && S.Bombs >= F.BombUse)
-                || ((F.T=='tele') && TeleMod!=false)
-                || ((F.T=='laser') && LaserMod!=false)
-            )    $('#gameOverlay').attr('class','cursorCross');
-        else    $('#gameOverlay').attr('class','cursorWait');
+        var useUse = true;
+        if(F.Use)
+            for(var useU in F.Use)
+                if(S.Storage[ useU ].R < F.Use[useU])
+                    useUse = false;
+
+
+        if(useUse || ((F.T=='tele') && TeleMod!=false) || ((F.T=='laser') && LaserMod!=false))
+            $('#gameOverlay').attr('class','cursorCross');
+        else
+            $('#gameOverlay').attr('class','cursorWait');
     }
 
     if(F.T=='missle' || F.T=='missleR')
@@ -278,43 +292,41 @@ GAMEobject.prototype.decide_ship = function(e){
     if(S.MouseDown1) aktywneDziala[ S.FireType ]=1;
     if(S.FireType2!==false && S.MouseDown2) aktywneDziala[ S.FireType2 ]=1;
     for(var dzi in aktywneDziala) if(S.FireTypes[dzi].gunS > S.FireTypes[dzi].GunSpeed){
+      var enoughToUse = true
+      if(F.Use) for(var useU in F.Use) if(S.Storage[ useU ].R < F.Use[useU]) enoughToUse = false;
+      if(!enoughToUse) continue;
+
+        var shotDone = false;
         var Fx = S.FireTypes[dzi];
-        if(Fx.T=='single' && S.Ammo >= Fx.AmmoUse){
+        if(Fx.T=='single'){
             this.shipShoot(0, Fx.Speed, Fx.Dec, Fx.Power);
-            Fx.gunS=0;
-            S.Ammo-=Fx.AmmoUse;
+            shotDone = true;
         }
-        if(Fx.T=='double' && S.Ammo >= Fx.AmmoUse){
+        if(Fx.T=='double'){
             this.shipShootOnSide(-90, 5, Fx.Speed, Fx.Dec, Fx.Power);
             this.shipShootOnSide(90, 5, Fx.Speed, Fx.Dec, Fx.Power);
-            Fx.gunS=0;
-            S.Ammo-=Fx.AmmoUse;
+            shotDone = true;
         }
-        if(Fx.T=='rose' && S.Ammo >= Fx.AmmoUse){
+        if(Fx.T=='rose'){
             for(var i = -parseInt(Fx.AtOnce/2); i<= parseInt(Fx.AtOnce/2); ++i)
                 this.shipShoot(i*Fx.RoseAngle, Fx.Speed, Fx.Dec, Fx.Power);
-            Fx.gunS=0;
-            S.Ammo-=Fx.AmmoUse;
+            shotDone = true;
         }
-        if(Fx.T=='missle' && S.Missles >= Fx.MissleUse && this.missleAim!=false){
+        if(Fx.T=='missle' && this.missleAim!=false){
             this.shipShootMissle(this.missleAim,O.angle,Fx.Speed,Fx.Dec,Fx.SpeedT,Fx.Power);
-            Fx.gunS=0;
-            S.Missles-=Fx.MissleUse;
+            shotDone = true;
         }
-        if(Fx.T=='missleR' && S.Missles >= Fx.MissleUse && this.missleAim!=false){
+        if(Fx.T=='missleR' && this.missleAim!=false){
             var Pe = [80,280,100,260,120,240,140,220,160,200,175,185];
             var Angle = parseInt(- (Math.atan2(this.mouseX-O.x,this.mouseY-O.y)*180/Math.PI)- -180)%360;
 
-
             for(var iki=0; iki<Fx.AtOnce; ++iki)
                 this.shipShootMissle(this.missleAim, (Angle- -Pe[iki])%360, (Fx.Speed-parseInt(iki/2)*2),(Fx.Dec- -parseInt(iki/2)*20),(Fx.SpeedT-parseInt(iki/2)),Fx.Power);
-            Fx.gunS=0;
-            S.Missles-=Fx.MissleUse;
+            shotDone = true;
         }
-        if(Fx.T=='bomb' && S.Bombs >= Fx.BombUse){
+        if(Fx.T=='bomb'){
             this.shipShootBomb(Fx.Speed,Fx.Dec,Fx.Power,Fx.Dist);
-            Fx.gunS=0;
-            S.Bombs-=Fx.BombUse;
+            shotDone = true;
         }
         if(Fx.T=='laser' && LaserMod!=false){
             --S.Modules[LaserMod].LaserLoad;
@@ -323,38 +335,36 @@ GAMEobject.prototype.decide_ship = function(e){
         }
         if(Fx.T=='tele' && TeleMod!=false)
             this.shipFunc_teleport(Fx,TeleMod);
+
+        if(shotDone){
+            Fx.gunS=0;
+            if(Fx.Use) for(var useU in Fx.Use) S.Storage[ useU ].R-=Fx.Use[ useU ];
+
+        }
     }
+
+
     F.gunS++;
     if(F2) F2.gunS++;
 
-    if(S.Ammo > S.AmmoStorage)      S.Ammo = S.AmmoStorage;
-    if(S.Missles > S.MissleStorage) S.Missles = S.MissleStorage;
-    if(S.Bombs > S.BombStorage)     S.Bombs = S.BombStorage;
+    for(var storU in S.Storage)
+        if(S.Storage[storU].R > S.Storage[storU].M)
+            S.Storage[storU].R = S.Storage[storU].M;
     O.ammo++;
 
-    if(S.Missles != Sx.missleStorage){
-        html='<span>';
-        for(var x=0; x<S.Missles; ++x) html+='Y';
-        html+='</span>';
-        for(var x=0; x<S.MissleStorage - S.Missles; ++x) html+='Y';
-        $('#missleStorage').html(html);
-        Sx.missleStorage = S.Missles;
-    }
-    if(S.Bombs != Sx.bombStorage){
-        html='<span>';
-        for(var x=0; x<S.Bombs; ++x) html+='P';
-        html+='</span>';
-        for(var x=0; x<S.BombStorage - S.Bombs; ++x) html+='P';
-        $('#bombStorage').html(html);
-        Sx.bombStorage = S.Bombs;
-    }
-    if(S.Ammo != Sx.ammoStorage){
-        html='<span>';
-        for(var x=0; x<S.Ammo; ++x) html+='i';
-        html+='</span>';
-        for(var x=0; x<S.AmmoStorage - S.Ammo; ++x) html+='i';
-        $('#ammoStorage').html(html);
-        Sx.ammoStorage = S.Ammo;
+    for(var storU in S.Storage)
+        if(S.Storage[storU].R != Sx.Storage[storU].R){
+            var letter = 'i';
+            if(storU=='Bomb') letter = 'P';
+            if(storU=='Missile') letter = 'Y';
 
-    }
+            var A = S.Storage[storU].R;
+            var B = S.Storage[storU].M - A;
+            html='<span>';
+            for(var x=0; x<A; ++x) html+=letter;
+            html+='</span>';
+            for(var x=0; x<B; ++x) html+=letter;
+            $('#'+storU+'Storage').html(html);
+            Sx.Storage[storU].R = S.Storage[storU].R;
+        }
 }
