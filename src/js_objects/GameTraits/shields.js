fe7089f6bdf7a49c3917d4851b8312db772be504
Przemek <przem.dro@gmail.com>
2016-10-07 13:00:09 +0200

    shields work?

--- src/js_objects/GameTraits/shields.js
+++ src/js_objects/GameTraits/shields.js
@@ -1,48 +1,86 @@
-GAMEobject.prototype.addShield = function(){}
-GAMEobject.prototype.checkShields = function(){}
-GAMEobject.prototype.removeShields = function(){}
+GAMEobject.prototype.addShield = function(){
+
+
+}
+GAMEobject.prototype.checkShields = function(O,o){
+    var sh,SH;
+    for(sh in O.Shields){
+        SH = O.Shields[sh];
+        if(SH.ExpireTime != 'infinite' && SH.ExpireTime < this.tick){
+            this.removeShield(O,o,sh);
+        }
+    }
+}
+GAMEobject.prototype.removeShield = function(O,o,sh){
+
+
+}
 
 GAMEobject.prototype.testShields = function(O,o,DMG){
-
     var DMGtype = DMG.T;
     var DMGval = DMG.Dmg;
     var ShieldHits = 0;
 
+    var sh,SH,DMGmin,DMGMaxReduce,DMGreduce,DMGReduceMax,DMGpercent,P;
 
     for(var sh in O.Shields){
         var SH = O.Shields[sh];
         if(SH.CatchDmgT[DMGtype]){
 
+
             if(SH.DmgReduction){
-                if(SH.DmgReduction == 'infinite'){
-                    ShieldHits = DMGval;
-                    DMGval = 0;
-                }else{
-                    var DMGmin = Math.min(DMGval,SH.DmgReduction);
-                    DMGval -= DMGmin;
-                    O.Shields[sh].DmgReduction -= DMGmin;
-                    ShieldHits = DMGmin;
+
+                DMGreduce = 99999;
+                // Partial Reduction
+                if(SH.PartialReduction){
+                    P = SH.PartialReduction;
+                    if(DMGval > P.MinLeft){
+                        DMGReduceMax = DMGval - P.MinLeft;
+                        DMGMaxReduce = P.Reduce;
+                        if(P.Reduce == 'infinite') DMGreduce = DMGReduceMax;
+                                else               DMGreduce = Math.min(DMGReduceMax, P.Reduce);
+                        if(P.MaxPercent)
+                            DMGreduce = Math.min(DMGreduce, P.Reduce, parseInt(DMGval*P.MaxPercent/100));
+                    }
                 }
-            }
 
-            if(SH.PartialReduction){
-                var P = SH.PartialReduction;
-                if(DMGval > P.MinLeft){
+                if(SH.DmgReduction == 'infinite'){
+                    DMGreduce = Math.min(DMGreduce, DMGval);
 
+                    ShieldHits-=-DMGreduce;
+                    DMGval -= DMGreduce;
+                }else{
+                    DMGreduce = Math.min(DMGval, SH.DmgReduction, DMGreduce);
+
+                    ShieldHits-=-DMGreduce;
+                    DMGval -= DMGreduce;
+                    O.Shields[sh].DmgReduction -= DMGreduce;
                 }
             }
 
             if(SH.ReductionUses != 'infinite'){
                 --O.Shields[sh].ReductionUses;
             }
-            // DmgTransfer: true,
-            // ExpireTime: -1 / 2939,
-            // HitActionObj: 'die' / 'redirect' / 'ignore',
-            // PartialReduction:{Reduce:5, MaxPercent:70 MinLeft:3}
+
+            if(SH.DmgTransfer){
+
+            }
+
+            // {
+            //     CatchDmgT: {bullet:1, energy:1, acid:1},
+            //     PartialReduction: {MinLeft: 1, Reduce: 4, MaxPercent: 30},
+            //     DmgReduction: 'infinite' / 12,
+            //     ReductionUses: 'infinite' / 1,
+            //     DmgTransfer: 'O[id]',
+            //     ExpireTime: 'infinite' / , expireTick,
+            // }
 
 
-            if(SH.DmgReduction==0 || SH.ReductionUses==0){}
+            if(SH.DmgReduction==0 || SH.ReductionUses==0){
+                this.removeShield(O,o,sh);
+            }
         }
+        // All damage reduced
         if(DMGval < 1) break;
     }
 
