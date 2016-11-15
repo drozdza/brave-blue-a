082ddf29e97fd4a7a1219ebd111cda997c1493b1
Przemek <przem.dro@gmail.com>
2016-11-15 23:57:28 +0100

    Campain Manager Scale

--- src/js_objects/CampainManagerObject.js
+++ src/js_objects/CampainManagerObject.js
@@ -12,40 +12,62 @@ CampainManagerObject = function(){
 
     this.completeLevel = function(levelName, partialsTab){
         console.log(levelName, partialsTab);
-        if(typeof this.levelsCompletion[levelName] == 'undefined')
-            this.levelsCompletion[levelName] = {partials:[]};
+        if(partialsTab.search('W') != -1){
+            if(typeof this.levelsCompletion[levelName] == 'undefined')
+                this.levelsCompletion[levelName] = {partials:[]};
 
-        var l = this.levelsCompletion[levelName].partials.length;
-        this.levelsCompletion[levelName].partials[ l ] = partialsTab;
+            var l = this.levelsCompletion[levelName].partials.length;
+            this.levelsCompletion[levelName].partials[ l ] = partialsTab;
 
-        this.scalePartials(levelName);
+            this.logPartials();
 
-        console.log(this.levelsCompletion);
-        this.saveCampain();
-        this.countGoldAndFlags();
+            this.scalePartials(levelName);
+
+            this.logPartials();
+
+            this.saveCampain();
+            this.countGoldAndFlags();
+        }
+    }
+
+    this.logPartials = function(){ // to later remove
+        for(var i in this.levelsCompletion){
+            var L = this.levelsCompletion[i];
+            for(var j in L.partials){
+                console.log(i+' '+L.partials[j]);
+            }
+        }
     }
 
     this.scalePartials = function(levelName){
         var P = this.levelsCompletion[levelName].partials;
 
-        var toRemove = {};
+        var e = 1;
+        while((e = this.scalePartials_find(P))!==false){
+            var vP = [];
+            for(var a in P){
+                if(a != e)
+                    vP[ vP.length ] = P[a];
+            }
+            P = vP;
+        }
+        this.levelsCompletion[levelName].partials = P;
+    }
+
+    this.scalePartials_find = function(P){
         for(var a in P)
             for(var b in P){
+                if(a==b) continue;
+                console.log('Porownajmy:',P[a],P[b]);
                 var isIn = true;
-                for(var i in P[a])
+                for(var i=0; i<P[a].length; ++i)
                     if(P[a][i]=='W' && P[b][i]=='L'){
                         isIn = false;
                         break;
                     }
-                if(isIn) toRemove[a] = true;
+                if(isIn) return a;
             }
-
-        var vP = [];
-        for(var a in P){
-            if(typeof toRemove[a] == 'undefined')
-                vP[ vP.length ] = P[a];
-        }
-        this.levelsCompletion[levelName].partials = vP;
+        return false;
     }
 
     this.getSavedCampain = function(){
@@ -60,17 +82,18 @@ CampainManagerObject = function(){
         var cookie = '';
         for(var i in this.levelsCompletion){
             var C = this.levelsCompletion[i];
-            cookie += '|'+i+':';
+            cookie += '|'+i+'-';
             for(var p in C.partials){
                 var P = C.partials[p];
                 for(var t in P){
                     if(P[t]=='W') cookie += 'W';
                         else      cookie += 'L';
                 }
-                cookie += ':';
+                cookie += '-';
             }
         }
         setCookie('MistForge_BraveBlueA',cookie,1000);
+        console.log(cookie);
     }
 
     this.countGoldAndFlags = function(){
