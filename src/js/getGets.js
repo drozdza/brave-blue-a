64d4fb4ce0c9133997e86c831be5e940ed560ed6
Przemek <przem.dro@gmail.com>
2017-08-20 17:43:21 +0200

    improved debug menu

--- src/js/getGets.js
+++ src/js/getGets.js
@@ -1,15 +1,14 @@
 BBAdata.GET={
-    DEBUG:0,
     MENU:0,
+    DEBUG:0,
     BLUR:0,
-    FRAMES:0,
+    FRAMES:4,
     CANVAS:0,
     FPS:0,
     SETF:30,
     GAMESTATS: 0,
     MAPSMODE: 0,
     SHIPPRESET: 0,
-
 };
 (function(){
     var GETs = document.location.href.split('?')[1];
@@ -18,6 +17,7 @@ BBAdata.GET={
         for(var gi=0; gi<GETs.length; ++gi){
             var BUM = GETs[gi].split('=');
             switch(BUM[0]){
+                case 'menu':         BBAdata.GET.MENU         = BUM[1]; break;
                 case 'debug':        BBAdata.GET.DEBUG        = BUM[1]; break;
                 case 'blur':         BBAdata.GET.BLUR         = BUM[1]; break;
                 case 'frames':       BBAdata.GET.FRAMES       = BUM[1]; break;
@@ -27,7 +27,6 @@ BBAdata.GET={
                 case 'gameStats':    BBAdata.GET.GAMESTATS    = BUM[1]; break;
                 case 'mapsMode':     BBAdata.GET.MAPSMODE     = BUM[1]; break;
                 case 'shipPresets':  BBAdata.GET.SHIPPRESET   = BUM[1]; break;
-                case 'menu':         BBAdata.GET.MENU         = BUM[1]; break;
             }
         }
     }
--- src/js_objects/GetMenuObject.js
+++ src/js_objects/GetMenuObject.js
@@ -1,50 +1,60 @@
 function GetMenuObject(){
 
     this.data={
+        MENU:{
+            get: 'menu',
+            values: {Off:0, On:1},
+            default: 0,
+        },
         DEBUG:{
             get: 'debug',
-            values: {Off:false, On:1,Ts:2,TsF:3},
+            values: {Off:0, On:1,Ts:2,TsF:3},
+            default: 0,
         },
         BLUR:{
             get: 'blur',
-            values: {Off:false, On:1, Align:2},
+            values: {Off:0, On:1, Align:2},
+            default: 0,
         },
         FRAMES:{
             get: 'frames',
-            values: {Max:false, M1:1, M2:2, M3:3, M4:4},
+            values: {Max:0, M1:1, M2:2, M3:3, M4:4},
+            default: 4,
         },
         CANVAS:{
             get: 'canvas',
             values: {Off:false, On:1, BG:2},
+            default: false,
         },
         FPS:{
             get: 'fps',
             values: {Off:false, Minimal:1, Graph:2, Max:3},
+            default: false,
         },
         SETF:{
             get: 'setFrames',
-            values: {'5':5, '10':10, '20':20, '30':false, '45':45},
+            values: {'5':5, '10':10, '20':20, '30':30, '45':45},
+            default: 30,
         },
         GAMESTATS:{
             get: 'gameStats',
             values: {Off:false, On:1},
+            default: false,
         },
         MAPSMODE:{
             get: 'mapsMode',
             values: {Campain:false, Test:1},
+            default: false,
         },
         SHIPPRESET:{
             get: 'shipPresets',
-            values: {start:false, destFields: 'destFields', bombs1: 'bombs1', bombs2: 'bombs2', best: 'best', bombardier: 'bombardier', bombardier2: 'bombardier2', ethernal: 'ethernal'},
+            values: {start: 0, destFields: 'destFields', bombs1: 'bombs1', bombs2: 'bombs2', best: 'best', bombardier: 'bombardier', bombardier2: 'bombardier2', ethernal: 'ethernal'},
+            default: 0,
         },
-        MENU:{
-            get: 'menu',
-            values: false,
-        }
     };
 
     this.makeHtml = function(){
-        if(BBAdata.GET.MENU == 0) return '';
+        if(BBAdata.GET.MENU == 0) return '<div id="hiddenMenu">'+this.makeLink('menu', 1, 'MENU')+'</div>';
         var html='<div id="GET_MENU">';
 
         for(var i in this.data){
@@ -62,18 +72,18 @@ function GetMenuObject(){
         var link = '?';
         var current = '';
 
-        for(var getD in BBAdata.GET){
-            var getN = this.data[getD].get;
-            var get = BBAdata.GET[getD];
+        for(var iGetData in BBAdata.GET){
+            var iGet = this.data[iGetData];
+            var getValue = BBAdata.GET[iGetData];
 
-            if(getName == getN){
-                if(value == get) current = 'class="current"';
-                if(getN=='setFrames' && get==30 && !value) current = 'class="current"';
+            if(getName == iGet.get){
+                if(value == getValue) current = 'class="current"';
+                if(iGet.get=='setFrames' && getValue==30 && !value) current = 'class="current"';
                 if(value != false)
-                  link+=getN+'='+value+'&';
+                  link+=iGet.get+'='+value+'&';
             }else{
-                if(get != 0 && !(getN=='setFrames' && get==30))
-                    link+=getN+'='+get+'&';
+                if(getValue != iGet.default && !(iGet.get=='setFrames' && getValue==30))
+                    link+=iGet.get+'='+getValue+'&';
             }
         }
 
